572efc0f938216c221f7c5362f265d7eb59b688e
Alejandro Sarasti Sierra <54252892+sarasti2@users.noreply.github.com>
2024-04-28 21:50:37 -0500

    Update predict.py

--- fase-2/scripts/predict.py
+++ fase-2/scripts/predict.py
@@ -27,7 +27,7 @@ if not os.path.isfile(input_file):
     
     
 logger.info("loading input data")
-Xts = pd.read_csv(input_file).values
+Xts = pd.read_csv(input_file).values[:,:-1]
 
 
 logger.info("loading model")
