759503d4c33db740bc9d8e43a19da3efb65ef833
Alejandro Sarasti Sierra <54252892+sarasti2@users.noreply.github.com>
2024-04-28 21:38:20 -0500

    Update train.py

--- fase-2/scripts/train.py
+++ fase-2/scripts/train.py
@@ -31,7 +31,7 @@ ytr = z[:,-1]
 
 logger.info("fitting model")
 gbm = xgb.XGBClassifier(max_depth=3, n_estimators=10, learning_rate=0.05, use_label_encoder=False)
-gbm = gbm.fit(X_train, y_train)
+gbm = gbm.fit(Xtr, ytr)
 
 logger.info(f"saving model to {model_file}")
 with open(model_file, "wb") as f:
